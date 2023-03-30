70348523e6499ac8c5eaf14e81eb34cb47fe3264
Shai Halimi <shalimi@c2r8s5.42lausanne.ch>
2023-03-30 16:11:23 +0200

    fixed split

--- src/utils/split.cpp
+++ src/utils/split.cpp
@@ -6,7 +6,7 @@
 /*   By: shalimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 16:11:35 by shalimi           #+#    #+#             */
-/*   Updated: 2023/03/29 19:56:24 by shalimi          ###   ########.fr       */
+/*   Updated: 2023/03/30 16:10:49 by shalimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ std::vector<std::string> split(std::string const & str, char const	* sep)
 {
 	return split(str, std::string(sep));
 }
+
 std::vector<std::string> split(std::string const & str, std::string const & sep)
 {
 	std::vector<std::string> ret;
@@ -23,14 +24,16 @@ std::vector<std::string> split(std::string const & str, std::string const & sep)
 	size_t	start = 0;
 	size_t	index = str.find(sep, start);
 
-	do
+	while (start != str.size())
 	{
+
 		tmp = str.substr((start > str.size()) ? str.size() : start, index - start);
 		start = index + sep.size();
 		ret.push_back(tmp);
+		if (index == std::string::npos) break;
 		index = str.find(sep, start);
-	} while (index != std::string::npos);
-	if (start < str.size() - 1)
-		ret.push_back(str.substr(start, str.size()));
+	}
+	//if (start < str.size() - 1)
+	//	ret.push_back(str.substr(start, str.size()));
 	return (ret);
 }
